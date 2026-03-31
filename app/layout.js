8c95614a2f96d848bf91f929a530cc4d183447b8
MrExxe <enicolas.dm@gmail.com>
2026-03-31 21:48:35 +0200

    improve typography with INTER

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,9 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Lean On | Sales Playbooks para startups early-stage",
@@ -9,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
